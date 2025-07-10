ded08ba78947ffab0f1ecb5860e6a8851ba07908
rafael4501 <rfledvaldo@gmail.com>
2025-07-09 22:39:19 -0300

    Update Usuario.php
    Alteração Usuário VO

--- modelo/vo/Usuario.php
+++ modelo/vo/Usuario.php
@@ -1,11 +1,60 @@
 <?php
 class Usuario {
-   private $id;
-   private $login;
-   private $senha;
+    private $id;
+    private $login;
+    private $senha;
     private $nome;
     private $email;
+    private $foto;
 
 
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getLogin() {
+        return $this->login;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getFoto() {
+        return $this->foto;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setLogin($login) {
+        $this->login = $login;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setFoto($foto) {
+        $this->foto = $foto;
+    }
 }
 ?>
+
